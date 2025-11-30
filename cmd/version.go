db115dc5a58ddac313219432625172abb475f6ea
Umang Hirani <umanghirani.exe@gmail.com>
2025-11-30 16:24:48 +0530

    feat(versionCmd) : added the initial implementaion of the version command

---
+++ cmd/version.go
@@ -0,0 +1,30 @@
+/*
+Copyright © 2025 Umang Hirani umanghirani.exe@gmail.com
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+const VERSION = "v0.00"
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "prints out the current version of the application",
+	Long:  `this command tells you the current version of the application`,
+
+	// TODO: find out how to make the --version and the -V the aliases for the version command
+	Aliases: []string{"--version", "-V"},
+
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(VERSION)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
