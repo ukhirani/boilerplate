b5fb884fd246637df650da19fe157e101a3f2246
Umang Hirani <umanghirani.exe@gmail.com>
2025-12-01 22:42:23 +0530

    feat(root) : removed the default behaviour to show help

--- cmd/root.go
+++ cmd/root.go
@@ -9,19 +9,22 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
+var (
+	version bool
+)
+
 var rootCmd = &cobra.Command{
 	Use:   "bp",
 	Short: "A cli tool to ironically skip the boilerplate",
 	Long:  "A cli tool to ironically skip the boilerplate",
 
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: RootCmdRunner,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
+func RootCmdRunner(cmd *cobra.Command, args []string) {
+
+}
+
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -30,13 +33,5 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.bp.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	//rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().BoolVarP(&version, "version", "v", false, versionCmd.Short)
 }
