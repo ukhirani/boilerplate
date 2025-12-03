fa3acd26701acb17b2d90c05830881b772a6e85a
Umang Hirani <umanghirani.exe@gmail.com>
2025-12-03 20:17:50 +0530

    refactor(constants) : moved the version to the constants.go

--- cmd/version.go
+++ cmd/version.go
@@ -4,12 +4,11 @@ Copyright © 2025 Umang Hirani umanghirani.exe@gmail.com
 package cmd
 
 import (
+	"bp/constants"
 	"fmt"
 
 	"github.com/spf13/cobra"
 )
-
-const VERSION = "v0.00"
 
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
@@ -17,7 +16,7 @@ var versionCmd = &cobra.Command{
 	Short: "prints out the current version of the application",
 	Long:  `this command tells you the current version of the application`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(VERSION)
+		fmt.Println(constants.VERSION)
 	},
 }
 
--- constants/constants.go
+++ constants/constants.go
@@ -3,4 +3,5 @@ package constants
 const (
 	BOILERPLATE_DIR = "boilerplate"
 	TEMPLATE_DIR    = "templates"
+	VERSION         = "v0.00"
 )
