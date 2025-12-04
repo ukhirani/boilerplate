76a2866431f3ebb0fca6b02c3d0d975a6da98aa3
Umang Hirani <umanghirani.exe@gmail.com>
2025-12-04 12:03:06 +0530

    feat(utils) : integrated copyFile function

---
+++ utils/copy-file.go
@@ -0,0 +1,47 @@
+package utils
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+func CopyFile(srcDir, destDir, fileName string) error {
+
+	// Open the source file for reading.
+	sourceFile, err := os.Open(srcDir)
+	if err != nil {
+		return fmt.Errorf("failed to open source file '%s': %w", srcDir, err)
+	}
+	defer sourceFile.Close()
+
+	//create template directory
+	err = os.MkdirAll(destDir, os.ModePerm)
+	if err != nil {
+		fmt.Println("Falied to Create Directory : ", destDir)
+		fmt.Println("Error Message :", err)
+		os.Exit(1)
+	}
+
+	destDir = filepath.Join(destDir, fileName)
+
+	if isDestExists, err := Exists(destDir); isDestExists {
+		return fmt.Errorf("File already exists, choose a different destination file : %s", err)
+	}
+
+	// Create the destination file for writing.
+	destinationFile, err := os.Create(destDir)
+	if err != nil {
+		return fmt.Errorf("failed to create destination file '%s': %w", destDir, err)
+	}
+	defer destinationFile.Close() // Ensure the destination file is closed.
+
+	// Copy the contents from the source file to the destination file.
+	_, err = io.Copy(destinationFile, sourceFile)
+	if err != nil {
+		return fmt.Errorf("failed to copy file contents from '%s' to '%s': %w", srcDir, destDir, err)
+	}
+
+	return nil // Return nil if the copy was successful.
+}
