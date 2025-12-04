4b31fe145eafd470079f3475b649c2af2f3c5e3f
Umang Hirani <umanghirani.exe@gmail.com>
2025-12-04 17:06:43 +0530

    feat(gen) : integrated the copy template here in generate command

--- cmd/generate.go
+++ cmd/generate.go
@@ -17,19 +17,25 @@ var (
 )
 
 func GenerateCmdRunner(cmd *cobra.Command, args []string) {
-	//TODO: copy the damn files
 
 	// we are assured we only have one arguments
 	templateName := args[0]
 
+	//check whether the template exists or not
 	templateExists, _, templateDir := utils.IsTemplateExists(templateName)
 
 	if !templateExists {
 		fmt.Printf("Template %v Doesn't Exist at %v", templateName, templateDir)
 		os.Exit(1)
 	}
-
 	fmt.Printf("Template %v Exist at %v", templateName, templateDir)
+
+	//copy template in the current directory
+	err := utils.CopyTemplateHere(templateDir)
+	if err != nil {
+		fmt.Println("Error Copying Template: ", err)
+	}
+	fmt.Printf("Template %v recreated successfully !", templateName)
 
 }
 
@@ -47,9 +53,9 @@ func init() {
 	rootCmd.AddCommand(generateCmd)
 
 	//defining the flags
-	generateCmd.Flags().StringVarP(&generatedFileName, "name", "n", "", "The name of the generated file or directory")
+	generateCmd.Flags().StringVarP(&generatedFileName, "name", "n", "", "(not applicable for directories) The name of the generated file")
 	generateCmd.Flags().StringVarP(&generatedFileDir, "dir", "d", "", "(optional) The directory of the generated file or directory ")
 
 	//making the flags as required
-	generateCmd.MarkFlagRequired("name")
+	//generateCmd.MarkFlagRequired("name")
 }
