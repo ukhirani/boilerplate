97b1d5cddd34e9da5c46ce92739741c0606c66be
Umang Hirani <umanghirani.exe@gmail.com>
2025-12-05 22:55:16 +0530

    refactor(const) : refactored places where current directory was needed

--- constants/derived_constants.go
+++ constants/derived_constants.go
@@ -7,7 +7,8 @@ import (
 
 var HOME_DIR, _ = os.UserHomeDir()
 var USER_CONFIG_DIR, _ = os.UserConfigDir()
+var CURR_DIR, _ = os.Getwd()
 
 // Boilerplate Constants
 var BOILERPLATE_CONFIG_DIR = filepath.Join(HOME_DIR, BOILERPLATE_DIR, CONFIG_DIR)
-var BOILERPLATE_TEMPLATE = filepath.Join(HOME_DIR, BOILERPLATE_DIR, TEMPLATE_DIR)
+var BOILERPLATE_TEMPLATE_DIR = filepath.Join(HOME_DIR, BOILERPLATE_DIR, TEMPLATE_DIR)
