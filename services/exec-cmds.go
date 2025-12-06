d62d73b3564f4cf1448d300c3c8bfa1f75e24d82
Umang Hirani <umanghirani.exe@gmail.com>
2025-12-07 00:08:24 +0530

    feat(services) : execCmds service implemented

---
+++ services/exec-cmds.go
@@ -0,0 +1,39 @@
+package services
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func ExecCmds(cmds []string) error {
+
+	for _, v := range cmds {
+
+		var cmdName string
+		var cmdArgs []string
+		splitCmd := strings.Split(v, " ")
+
+		cmdName = splitCmd[0]
+		if len(splitCmd) > 0 {
+			cmdArgs = splitCmd[1:]
+		}
+
+		cmd := exec.Command(cmdName, cmdArgs...)
+		fmt.Println(cmd)
+
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		if err := cmd.Run(); err != nil {
+			fmt.Println(err)
+			return err
+		}
+
+	}
+
+	return nil
+}
+
