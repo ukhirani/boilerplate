c0b15498ed5afb3dfe2c4e9909e325f448f0d75a
Umang Hirani <umanghirani.exe@gmail.com>
2025-12-23 13:46:43 +0530

    doc (clip) : added some comments to better elaborate the behaviour

--- cmd/clip.go
+++ cmd/clip.go
@@ -28,37 +28,43 @@ func ClipCmdRunner(cmd *cobra.Command, args []string) {
 	// check wheter the template exists or not
 	templateExists, templateDir := utils.IsTemplateExists(templateName)
 
+	// quit if template doesn't exist
 	if !templateExists {
 		fmt.Printf("Template [ %s ] doesn't exist. \n ", templateName)
 		os.Exit(1)
 	}
 
+	// read the config
 	var conf types.Config
 	if err := services.ReadConfig(templateName, &conf); err != nil {
 		fmt.Printf("Error reading config : %s .", err)
 		os.Exit(1)
 	}
 
+	// quit if template is type dir
 	if conf.IsDir {
 		fmt.Printf("Template [ %s ] is of type directory and can't be copied  \n ", templateName)
 		os.Exit(1)
 	}
 
+	// get the exact template file name, then to read the content inside it
 	templateFileName, err := utils.GetTemplateFileDir(templateName)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
+	// join the template file path and the file path
 	templateDir = filepath.Join(templateDir, templateFileName)
-	// fmt.Println(templateDir)
 
+	// read the file content
 	data, err := os.ReadFile(templateDir)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
+	// write to clipboard and exit if errors
 	if err = clipboard.WriteAll(string(data)); err != nil {
 		fmt.Println("error writing to clipboard :", err)
 		os.Exit(1)
