3e61af40f21e23076c5a694c25fd5feaec2549cf
Umang Hirani <umanghirani.exe@gmail.com>
2025-12-23 09:10:34 +0530

    feat (clip) : clipboard feature finally working for file type templates

--- cmd/clip.go
+++ cmd/clip.go
@@ -5,7 +5,10 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/atotto/clipboard"
 	"github.com/spf13/cobra"
+	"github.com/ukhirani/boilerplate/services"
+	"github.com/ukhirani/boilerplate/types"
 	"github.com/ukhirani/boilerplate/utils"
 )
 
@@ -19,6 +22,7 @@ var clipCmd = &cobra.Command{
 }
 
 func ClipCmdRunner(cmd *cobra.Command, args []string) {
+	// guaranteed that we have exactly one argument
 	templateName := args[0]
 
 	// check wheter the template exists or not
@@ -29,13 +33,33 @@ func ClipCmdRunner(cmd *cobra.Command, args []string) {
 		os.Exit(1)
 	}
 
+	var conf types.Config
+	services.ReadConfig(templateName, &conf)
+
+	if conf.IsDir {
+		fmt.Printf("Template [ %s ] is of type directory and can't be copied \n ", templateName)
+		os.Exit(1)
+	}
+
 	templateFileName, err := utils.GetTemplateFileDir(templateName)
 	if err != nil {
 		fmt.Println(err)
+		os.Exit(1)
 	}
 
 	templateDir = filepath.Join(templateDir, templateFileName)
 	fmt.Println(templateDir)
+
+	data, err := os.ReadFile(templateDir)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	if err = clipboard.WriteAll(string(data)); err != nil {
+		fmt.Println("error writing to clipboard :", err)
+		os.Exit(1)
+	}
 }
 
 func init() {
