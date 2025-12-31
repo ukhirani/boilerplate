296c56267a43a9a3550193d54e91090465d2b509
Umang Hirani <umanghirani.exe@gmail.com>
2025-12-31 23:52:01 +0530

    feat (services) : the list-dir service now supports showing file type (optionally through a boolean)

--- services/list-dir.go
+++ services/list-dir.go
@@ -3,16 +3,37 @@ package services
 import (
 	"fmt"
 	"os"
+
+	"github.com/ukhirani/boilerplate/types"
 )
 
 // TODO: can't we do a recursive call to nicely print out a tree
 
-func ListDir(path string) (int, error) {
+func ListDir(path string, showTypes bool) (int, error) {
 	entries, err := os.ReadDir(path)
 
 	for _, entry := range entries {
-		fmt.Printf("  • %s\n", entry.Name())
-	}
+		if showTypes {
+			// TODO: one more problem, we are just listing out the folders to see their names.
+			// This is limited in it's way to print out more info regarding any templates.
 
+			fileType := "FILE"
+
+			// now we have to read the configs of each templates and then find out the fileType
+			var conf types.Config
+			if err := ReadConfig(entry.Name(), &conf); err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+
+			if conf.IsDir {
+				fileType = "DIR"
+			}
+
+			fmt.Printf("[ %s ] %s\n", fileType, entry.Name())
+		} else {
+			fmt.Printf("• %s\n", entry.Name())
+		}
+	}
 	return len(entries), err
 }
