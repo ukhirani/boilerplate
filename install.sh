c695ffe93664543814154db86bdc9c96b05d3698
Umang Hirani <umanghirani.exe@gmail.com>
2026-02-03 19:08:56 +0530

    Enhance install.sh with clearer messages and cleanup
    Updated installation script to provide clearer output and handle temporary directory more efficiently.

--- install.sh
+++ install.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 set -e
 
-echo "hello"
+echo "installing bp . . ."
+
 
 OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 ARCH=$(uname -m)
 
 case "$ARCH" in
-  x86_64) ARCH=amd64 ;;
-  aarch64 | arm64) ARCH=arm64 ;;
-  *) echo "Unsupported arch: $ARCH"; exit 1 ;;
+x86_64) ARCH=amd64 ;;
+aarch64 | arm64) ARCH=arm64 ;;
+*)
+  echo "Unsupported arch: $ARCH"
+  exit 1
+  ;;
 esac
 
 URL="https://github.com/ukhirani/boilerplate/releases/latest/download/bp_${OS}_${ARCH}.tar.gz"
 
-tmpdir=$(mktemp -d)
-curl -L "$URL" | tar -xz -C "$tmpdir"
-sudo mv "$tmpdir/bp" /usr/local/bin/bp
-rm -rf "$tmpdir"
-
-sudo mv bp /usr/local/bin
+curl -L "$URL" | tar -xz
+sudo mv bp /usr/local/bin/bp 2>/dev/null
 hash -r 2>/dev/null || true
 
 echo "bp installed successfully . . ."
