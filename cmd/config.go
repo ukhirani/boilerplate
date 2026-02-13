c2022eb1e4c73e74f4d0a7b8c47af65a2b06a13d
Umang Hirani <umanghirani.exe@gmail.com>
2026-02-13 15:45:41 +0530

    feat (config) : added the -e / --editor (optional) flag for overriding the editor that opens up the config file
    by default it will be vscode (code .)
    however it can be overriden by the -e flag
    
    Example :
    bp config <template-name> -e "nvim"
    bp config <template-name> --editor "vim"

--- cmd/config.go
+++ cmd/config.go
@@ -11,20 +11,31 @@ import (
 	"github.com/ukhirani/boilerplate/utils"
 )
 
-// var openEditor bool
+var customEditor string
 
 // configCmd represents the config command
 var configCmd = &cobra.Command{
 	Use:   "config",
 	Short: "used to configure your templates",
 	Long:  `used to configure your templates`,
-	Run:   ConifgCmdRunner,
+	RunE:  ConifgCmdRunner,
 	Args:  cobra.ExactArgs(1),
 }
 
-func ConifgCmdRunner(cobra *cobra.Command, args []string) {
+func ConifgCmdRunner(cmd *cobra.Command, args []string) error {
 	// get the first arg which will be the template name
 	templateName := args[0]
+
+	// use default editor, if no custom editor is passed via --editor flag
+	editorOfChoice := constants.DEFAULT_EDITOR
+	if cmd.Flags().Changed("editor") {
+		editorOfChoice = customEditor
+	}
+
+	// can't let the -e/--editor flag be empty (because you can't open any editor with that cmd)
+	if editorOfChoice == "" {
+		return fmt.Errorf("--editor cannot be empty")
+	}
 
 	// check if template exists
 	if ok, _ := utils.IsTemplateExists(templateName); !ok {
@@ -34,15 +45,18 @@ func ConifgCmdRunner(cobra *cobra.Command, args []string) {
 
 	templateConfigLocation := utils.GetConfigFileLocation(templateName)
 
-	if err := services.ExecCmds([]string{strings.Join([]string{constants.DEFAULT_EDITOR, templateConfigLocation}, " ")}); err != nil {
+	cmdToOpenEditor := strings.Join([]string{editorOfChoice, templateConfigLocation}, " ")
+	if err := services.ExecCmds([]string{cmdToOpenEditor}); err != nil {
 		fmt.Println("Error opening config", err)
 		os.Exit(1)
 	}
+
+	return nil
 }
 
 func init() {
 	rootCmd.AddCommand(configCmd)
 
 	// declaring the flags
-	// configCmd.Flags().BoolVarP(&openEditor, "edit", "e", false, "used to open your configs in your editor")
+	configCmd.Flags().StringVarP(&customEditor, "editor", "e", "", "used to open your configs in your editor")
 }
